231dadd8b523c4c6cc1a1694aee97b6fa05fe7fc
dgkhoa89 <dgkhoa89@gmail.com>
2022-02-23 14:57:20 +0700

    create items's window - v1

--- QuanLyKho/ViewModel/MainViewModel.cs
+++ QuanLyKho/ViewModel/MainViewModel.cs
@@ -12,14 +12,48 @@ namespace QuanLyKho.ViewModel
     {
         // mọi thứ xử lý sẽ nằm trong này
 
-        public ICommand LoadedWindowCommand { get; set; }
         public bool IsLoaded { get; set; } = false;
+        public ICommand LoadedWindowCommand { get; set; }
+        public ICommand UnitCommand { get; set; }
+        public ICommand SupplierCommand { get; set; }
+        public ICommand CustomerCommand { get; set; }
+        public ICommand ProductCommand { get; set; }
         public MainViewModel()
         {
-            LoadedWindowCommand = new RelayCommand<object>((p) => true, (p) => { 
+            LoadedWindowCommand = new RelayCommand<object>((p) => true, (p) =>
+            {
                 IsLoaded = true;
                 LoginWindow loginWindow = new LoginWindow();
                 loginWindow.ShowDialog();
+            });
+
+            UnitCommand = new RelayCommand<object>(
+                (p) => true,
+                (p) =>
+                {
+                    UnitWindow wd = new UnitWindow();
+                    wd.ShowDialog();
+                });
+            SupplierCommand = new RelayCommand<object>(
+                (p) => true,
+                (p) =>
+                {
+                    SupplierWindow wd = new SupplierWindow();
+                    wd.ShowDialog();
+                });
+            CustomerCommand = new RelayCommand<object>(
+            (p) => true,
+            (p) =>
+            {
+                CustomerWindow wd = new CustomerWindow();
+                wd.ShowDialog();
+            });
+            ProductCommand = new RelayCommand<object>(
+            (p) => true,
+            (p) =>
+            {
+                CustomerWindow wd = new CustomerWindow();
+                wd.ShowDialog();
             });
         }
     }
