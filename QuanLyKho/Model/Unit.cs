4a083337c2d98cafefcf8fa4fe1dc3f491837c9f
dgkhoa89_local <dgkhoa89@gmail.com>
2022-02-27 13:21:38 +0700

    v14

--- QuanLyKho/Model/Unit.cs
+++ QuanLyKho/Model/Unit.cs
@@ -9,10 +9,11 @@
 
 namespace QuanLyKho.Model
 {
+    using QuanLyKho.ViewModel;
     using System;
     using System.Collections.Generic;
     
-    public partial class Unit
+    public partial class Unit:BaseViewModel
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public Unit()
@@ -21,7 +22,11 @@ namespace QuanLyKho.Model
         }
     
         public int Id { get; set; }
-        public string DisplayName { get; set; }
+        private string _DisplayName;
+        public string DisplayName {
+            get { return _DisplayName; }
+            set { _DisplayName = value; OnPropertyChanged(); }
+                }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Product> Products { get; set; }
